5dab79eb34a5a870f46fd7ea4659ed2d9a80e3a3
touch4645 <k.terazawa@lillyholdings.co.jp>
2020-01-08 17:57:39 +0900

    first commit

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # 本番運用時は別の値に置き換えること。
 # 生成ツール：https://www.miniwebtool.com/django-secret-key-generator/
-SECRET_KEY = '_(u)&e-3076d7_a(gfhb85*)u-u4vwc%#qt7c7q3-t98%lvp&g'
+SECRET_KEY = 'j*2682y=c_h%+(p54186bedw+q606ylji2nao#^*r46_91_)37'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
